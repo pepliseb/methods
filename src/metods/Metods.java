27d015280a032c4666019009eb5b0e6f39eeebd6
eman <eman@onet.eu>
2017-07-29 15:43:44 +0200

    ssss

--- src/metods/Metods.java
+++ src/metods/Metods.java
@@ -52,7 +52,7 @@ public class Metods {
         return result;
     }
 
-    private boolean sqrInt (int a ){
+    private boolean sqrInt (int a2 ){
         if (a==1) return true;
         for(int i=2;i<=a;i++)
        if( Math.pow(i,2)  == a)  return true;
